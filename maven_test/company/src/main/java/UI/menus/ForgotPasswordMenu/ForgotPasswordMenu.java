5f607f7cf9732cdcc5ae4b57c8a8a34cae129d95
iggy12345 <aaronjencks@gmail.com>
2019-10-31 01:04:48 -0500

    Finished draft for password reset

--- maven_test/company/src/main/java/UI/menus/ForgotPasswordMenu/ForgotPasswordMenu.java
+++ maven_test/company/src/main/java/UI/menus/ForgotPasswordMenu/ForgotPasswordMenu.java
@@ -12,6 +12,7 @@ public class ForgotPasswordMenu extends LoginMenu {
     protected String security_question = new String();
     protected String security_answer = new String();
     protected String confirm_password = new String();
+    protected int iteration = 0;
 
     public ForgotPasswordMenu(ITermController parent) { super(parent); }
 
@@ -28,9 +29,12 @@ public class ForgotPasswordMenu extends LoginMenu {
         String new_prompt = new String();
         for(int i = 0; i < h_pad; i++)
             new_prompt += " ";
-        prompt = new_prompt + ((username.isEmpty()) ? "Username: " : 
-                               ("Password: ");
-
+        prompt = new_prompt + ((iteration == 0) ? "Username: " :
+                (iteration == 1) ? "Security Question: " + security_question :
+                        (iteration == 2) ? "New Password: " : "Confirm Password: ");
+        if(iteration++ == 3)
+            iteration = 0;
+            
         return result;
     }
 
@@ -46,12 +50,23 @@ public class ForgotPasswordMenu extends LoginMenu {
         try { username = UIUtil.get_input(ForgotPasswordMenu.inputScanner, username, prompt, (String s) -> { return true; }); }
         catch(Exception e) { e.printStackTrace(); }
 
+        // TODO Fetch user security question
         get_display_string();
+        System.out.println(prompt);
 
-        try { password = UIUtil.get_input(ForgotPasswordMenu.inputScanner, password, prompt, (String s) -> { return true; }); }
+        get_display_string();
+        try { security_answer = UIUtil.get_input(ForgotPasswordMenu.inputScanner, security_answer, prompt, (String s) -> { return true; }); }
         catch(Exception e) { e.printStackTrace(); }
 
         // TODO check existence
+
+        get_display_string();
+        try { password = UIUtil.get_input(ForgotPasswordMenu.inputScanner, password, prompt, (String s) -> { return true; }); }
+        catch(Exception e) { e.printStackTrace(); }
+
+        get_display_string();
+        try { confirm_password = UIUtil.get_input(ForgotPasswordMenu.inputScanner, confirm_password, prompt, (String s) -> { return true; }); }
+        catch(Exception e) { e.printStackTrace(); }
 
         return new MainMenuItem(parent);
     }
