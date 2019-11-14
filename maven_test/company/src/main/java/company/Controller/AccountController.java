1dfb2e056e0bb92661ee7da6f4b79a91ff95308e
iggy12345 <aaronjencks@gmail.com>
2019-11-14 01:04:45 -0600

    Made AccountController into singleton

--- maven_test/company/src/main/java/company/Controller/AccountController.java
+++ maven_test/company/src/main/java/company/Controller/AccountController.java
@@ -1,9 +1,23 @@
 package company.Controller;
 
 import company.Controller.Interface.IAccountController;
+import company.Entity.Vault;
 import company.Entity.Interface.IAccount;
+import company.Entity.Interface.ICreditAccount;
 
 public class AccountController implements IAccountController {
+
+    private static AccountController instance = null;
+
+    private AccountController() {}
+
+    public static AccountController getInstance()
+    {
+        if(instance == null)
+            instance = new AccountController();
+        return instance;
+    }
+
     @Override
     public void deposit(IAccount account, long amount)
     {
@@ -14,5 +28,12 @@ public class AccountController implements IAccountController {
     public void withdrawal(IAccount account, long amount)
     {
         // TODO
+    }
+
+    public void accrueInterest()
+    {
+        for(IAccount acct : Vault.getInstance().getAccounts().values())
+            if(ICreditAccount.class.isAssignableFrom(acct.getClass()))
+                ((ICreditAccount)acct).accrueInterest();
     }
 }
--- maven_test/company/src/main/java/company/Controller/Interface/IAccountController.java
+++ maven_test/company/src/main/java/company/Controller/Interface/IAccountController.java
@@ -1,8 +1,10 @@
 package company.Controller.Interface;
 
 import company.Entity.Interface.IAccount;
+import company.Entity.Interface.ICreditAccount;
 
 public interface IAccountController {
     public void deposit(IAccount account, long amount);
     public void withdrawal(IAccount account, long amount);
+    public void accrueInterest();
 }
