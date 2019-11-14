39f69eaf5d8a945f6b136e2c96648443c1d2ae51
iggy12345 <aaronjencks@gmail.com>
2019-11-14 00:37:23 -0600

    Added interest rate to credit accounts

--- maven_test/company/src/main/java/company/Entity/Abstract/ACreditAccount.java
+++ maven_test/company/src/main/java/company/Entity/Abstract/ACreditAccount.java
@@ -8,8 +8,25 @@ import company.Entity.Interface.ICreditAccount;
 import company.Entity.Interface.ISaveable;
 
 public abstract class ACreditAccount extends AAccount implements ICreditAccount {
+    private double interestRate;
+
     public static CreditAccount load(UUID id){
         Object o = ISaveable.load(CreditAccount.class, id);
         return (CreditAccount)o;
+    }
+
+    public double getInterestRate()
+    {
+        return interestRate;
+    }
+
+    public void setInterestRate(double rate)
+    {
+        interestRate = rate;
+    }
+
+    public void accrueInterest()
+    {
+        amount *= interestRate;
     }
 }
--- maven_test/company/src/main/java/company/Entity/Interface/ICreditAccount.java
+++ maven_test/company/src/main/java/company/Entity/Interface/ICreditAccount.java
@@ -1,4 +1,8 @@
 package company.Entity.Interface;
 
 public interface ICreditAccount extends IAccount{
+    public double getInterestRate();
+    public void setInterestRate(double rate);
+
+    public void accrueInterest();
 }
